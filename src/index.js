8cf2ab1d6bba3157e46d425f522e5aead217003b
Milota Styková <you@example.com>
2022-04-30 16:16:10 +0200

    getting data for drinks from api

--- src/index.js
+++ src/index.js
@@ -13,49 +13,19 @@ document.querySelector("nav").classList.toggle("nav-closed")
   })
 })
 
-const drinks = [
-  {
-    id: 'cappuccino',
-    name: 'Cappuccino',
-    ordered: false,
-    layers: [
-      {
-        color: '#feeeca',
-        label: 'mléčná pěna',
-      },
-      {
-        color: '#fed7b0',
-        label: 'teplé mléko',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/cappuccino.png',
-  },
-  {
-    id: 'romano',
-    name: 'Romano',
-    ordered: false,
-    layers: [
-      {
-        color: '#fbdf5b',
-        label: 'citrón',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-  },
-];
+fetch("https://apps.kodim.cz/daweb/cafelora/api/drinks")
+.then(response => response.json())
+.then(data => {
+  const drinks = data.results
+  for (let i = 0; i < drinks.length; i += 1) {
+    const addCoffee  = document.querySelector(".drinks-list")
+    addCoffee.appendChild(Drink(drinks[i]))
+    }
+})
 
-const addCoffee  = document.querySelector(".drinks-list")
-for (let i = 0; i < drinks.length; i += 1) {
-  addCoffee.appendChild(Drink(drinks[i]))
-}
+
+
+
 
 
 
