586a4679a4762e52c09f799dfcd8d2691b44eecb
Milota Styková <you@example.com>
2022-04-30 12:33:42 +0200

    adding component for coffee list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { Layer } from "./Layer/index.js"
 
 document.querySelector("#nav-btn").addEventListener("click", () => {
   document.querySelector("nav").classList.toggle("nav-closed")
@@ -18,15 +19,37 @@ const cup = document.querySelector(".drink__cup")
 let ordered = false
 
 orderButton.addEventListener("click", () => {
-  if (ordered === true) {
-    orderButton.textContent = "Objednat"
-    cup.classList.remove("drink__cup--selected")
-    ordered = false
-  }
-
-  else {
+  if (ordered === false) {
     orderButton.textContent = "Zrušit"
     cup.classList.add("drink__cup--selected")
     ordered = true
   }
+
+  else {
+    orderButton.textContent = "Objednat"
+    cup.classList.remove("drink__cup--selected")
+    ordered = false
+  }
   })
+
+  const coffees = [
+    {
+      color: '#feeeca',
+      label: 'mléčná pěna',
+    },
+    {
+      color: '#fed7b0',
+      label: 'teplé mléko',
+    },
+    {
+      color: '#613916',
+      label: 'espresso',
+    },
+  ]
+
+  const coffeeList = document.querySelector('.drink__info');
+  for (let i = 0; i < coffees.length; i += 1) {
+    coffeeList.innerHTML += Layer(coffees[i]);
+  }
+
+
