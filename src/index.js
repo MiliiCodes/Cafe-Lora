5b3218f44f568f854d2529676c2760914d9b6fa8
Milota Styková <you@example.com>
2022-04-30 12:01:06 +0200

    adding JS for ordering

--- src/index.js
+++ src/index.js
@@ -11,3 +11,22 @@ navItem.forEach((item) =>{
 document.querySelector("nav").classList.toggle("nav-closed")
   })
 })
+
+const orderButton = document.querySelector(".order-btn")
+const cup = document.querySelector(".drink__cup")
+
+let ordered = false
+
+orderButton.addEventListener("click", () => {
+  if (ordered === true) {
+    orderButton.textContent = "Objednat"
+    cup.classList.remove("drink__cup--selected")
+    ordered = false
+  }
+
+  else {
+    orderButton.textContent = "Zrušit"
+    cup.classList.add("drink__cup--selected")
+    ordered = true
+  }
+  })
