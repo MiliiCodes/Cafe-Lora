15ef64362d402be6547dc7f60dd76b48d7018e73
Milota Styková <you@example.com>
2022-04-30 11:38:05 +0200

    showing/hiding nav for mobile screen after clicking nav button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
 
-console.log('funguju!');
+document.querySelector("#nav-btn").addEventListener("click", () => {
+  document.querySelector("nav").classList.toggle("nav-closed")
+})
+
+const navItem = document.querySelectorAll("nav a")
+
+navItem.forEach((item) =>{
+  item.addEventListener("click", () => {
+document.querySelector("nav").classList.toggle("nav-closed")
+  })
+})
